630971925d36374fda42337963bbb75f30818f86
noahc12 <bringmethehorizon110@gmail.com>
2019-07-19 14:25:13 -0700

    Modal

--- src/app/modal/modal.page.ts
+++ src/app/modal/modal.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NavParams } from '@ionic/angular';
+import { ModalController } from '@ionic/angular';
+
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.page.html',
@@ -7,9 +9,17 @@ import { NavParams } from '@ionic/angular';
 })
 export class ModalPage implements OnInit {
 
-  constructor() { }
+  constructor(public modalController: ModalController) { }
 
   ngOnInit() {
+  }
+
+  dismiss() {
+    // using the injected ModalController this page
+    // can "dismiss" itself and optionally pass back data
+    this.modalController.dismiss({
+      dismissed: true
+    });
   }
 
 }
